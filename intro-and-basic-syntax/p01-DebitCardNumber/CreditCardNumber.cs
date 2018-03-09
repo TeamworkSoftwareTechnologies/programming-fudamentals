55b3688df53a6259f9ab97942059609ccbc208f1
naseood <naskostan4ev@gmail.com>
2018-03-09 16:34:50 +0200

    add problem 1(DebitCardNumber)
    Signed-off-by: naseood <naskostan4ev@gmail.com>

---
+++ intro-and-basic-syntax/p01-DebitCardNumber/CreditCardNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class CreditCardNumber
+{
+    static void Main(string[] args)
+    {
+        int firstNum = int.Parse(Console.ReadLine());
+        int secondNum = int.Parse(Console.ReadLine());
+        int tirdNum = int.Parse(Console.ReadLine());
+        int fourthNum = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{firstNum:D4} {secondNum:D4} {tirdNum:D4} {fourthNum:D4}");
+    }
+}
+
