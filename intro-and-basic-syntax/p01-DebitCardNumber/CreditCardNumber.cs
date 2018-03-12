5be4099564c7f91fce0fbf553362f4eb52873e60
sunday <kumanoff@abv.bg>
2018-03-12 16:32:45 +0200

    Added Comments

--- intro-and-basic-syntax/p01-DebitCardNumber/CreditCardNumber.cs
+++ intro-and-basic-syntax/p01-DebitCardNumber/CreditCardNumber.cs
@@ -5,11 +5,13 @@ class CreditCardNumber
 {
     static void Main(string[] args)
     {
+        //do stuff
         int firstNum = int.Parse(Console.ReadLine());
         int secondNum = int.Parse(Console.ReadLine());
         int tirdNum = int.Parse(Console.ReadLine());
         int fourthNum = int.Parse(Console.ReadLine());
 
+        //do more stuff
         Console.WriteLine($"{firstNum:D4} {secondNum:D4} {tirdNum:D4} {fourthNum:D4}");
     }
 }
