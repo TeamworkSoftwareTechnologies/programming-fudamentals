efdff7e2650ad0509354e417e97cb56e3125d945
Kristiyan Topchiyski <37207681+dmxice97@users.noreply.github.com>
2018-03-12 14:28:45 +0200

    Documenting the code of Problem 3
    Members responsibilities:
    b - Member 2 documents the code of Solution of Problem 3

--- intro-and-basic-syntax/exercise/03. Miles to kilometers/Program.cs
+++ intro-and-basic-syntax/exercise/03. Miles to kilometers/Program.cs
@@ -6,10 +6,14 @@ namespace _03._Miles_to_kilometers
     {
         static void Main(string[] args)
         {
+            //Declaring input variable
             double kilometeres = double.Parse(Console.ReadLine());
-            double miles = (kilometeres * 1.60934);
-
+            
+            //Converting from kilometeres to miles
             //1 mile == 1.60934 kilometers
+            double miles = (kilometeres * 1.60934);
+            
+            //Printing on Console converted variable    
             Console.WriteLine($"{miles:F2}");
         }
     }
