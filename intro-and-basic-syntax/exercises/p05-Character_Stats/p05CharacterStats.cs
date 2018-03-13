27140f74d981b02ab892bf1d1118de6e2b50f255
Nasko <naskostan4ev@gmail.com>
2018-03-13 07:46:20 +0200

    Update p05CharacterStats.cs

--- intro-and-basic-syntax/exercises/p05-Character_Stats/p05CharacterStats.cs
+++ intro-and-basic-syntax/exercises/p05-Character_Stats/p05CharacterStats.cs
@@ -6,20 +6,28 @@ namespace Character_Stats
     {
         static void Main(string[] args)
         {
+            //here we take the value from console
             var name = Console.ReadLine();
             int currentHealth = int.Parse(Console.ReadLine());
             int maxHealth = int.Parse(Console.ReadLine());
             int currentEnergy = int.Parse(Console.ReadLine());
             int maxEnergy = int.Parse(Console.ReadLine());
 
+            //set how many strips and dots we have made by values 
             var pipesHealth = new string('|', currentHealth);
             var dotsHealth = new string('.', maxHealth - currentHealth);
             var pipesEnergy = new string('|', currentEnergy);
             var dotsEnergy = new string('.', maxEnergy - currentEnergy);
 
+            //print the result
             Console.WriteLine("Name: " + name);
             Console.WriteLine("Health: |" + pipesHealth + dotsHealth + "|");
             Console.WriteLine("Energy: |" + pipesEnergy + dotsEnergy + "|");
+            
+            //result is something like that:
+            // |||||||...|
+            // |||||||||||
+            // ||........|
         }
     }
 }
