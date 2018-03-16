fefb441b15032c3bdc01b6d2a9ee89c9c29fa807
NapsterBG <napsterbg@mail.bg>
2018-03-16 17:46:44 +0200

    solved

---
+++ intro-and-basic-syntax/exercises/p04-BeverageLabels/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;  
+
+  
+
+namespace BeverageLabels  
+
+{  
+
+    class Program  
+
+    {  
+
+        static void Main(string[] args)  
+
+        {  
+
+            string foodname = Console.ReadLine();  
+
+            int foodvolume = int.Parse(Console.ReadLine()); 
+
+   
+
+            double energyContentPer100 = double.Parse(Console.ReadLine());  
+
+            double sugarContentPer100 = double.Parse(Console.ReadLine());  
+
+            Console.WriteLine($"{foodvolume}ml {foodname}:\r\n{energyContentPer100*foodvolume/100}kcal, {sugarContentPer100*foodvolume/100}g sugars");  
+
+        }  
+
+    }  
+
+}
