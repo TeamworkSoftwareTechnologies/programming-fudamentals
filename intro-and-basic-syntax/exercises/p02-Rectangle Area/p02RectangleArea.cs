0acc38817d36404ecad7bcf923c7cbad6a296475
NapsterBG <napsterbg@mail.bg>
2018-03-16 17:55:11 +0200

    commented

--- intro-and-basic-syntax/exercises/p02-Rectangle Area/p02RectangleArea.cs
+++ intro-and-basic-syntax/exercises/p02-Rectangle Area/p02RectangleArea.cs
@@ -7,11 +7,13 @@ namespace _02._Rectangle_Area
     {
         static void Main(string[] args)
         {
+            //Variable for width
             decimal width = decimal.Parse(Console.ReadLine());
+            //Variable for height
             decimal height = decimal.Parse(Console.ReadLine()); ;
-
+            //Area for calculation
             decimal area = width * height;
-
+            //Print the calculation
             Console.WriteLine("{0:f2}", area);
         }
     }
