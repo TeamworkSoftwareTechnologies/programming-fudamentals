3c668a57005d2248c6d83002d85ab4816e5c0433
Daniel-Takev <danieltakev@gmail.com>
2018-03-18 21:39:23 +0200

    Member 3 changes variable name in Problem 1.

--- intro-and-basic-syntax/exercises/p01-DebitCardNumber/p01CreditCardNumber.cs
+++ intro-and-basic-syntax/exercises/p01-DebitCardNumber/p01CreditCardNumber.cs
@@ -6,10 +6,10 @@ class CreditCardNumber
     static void Main(string[] args)
     {
         //here we take the names of person
-        int firstNum = int.Parse(Console.ReadLine());
-        int secondNum = int.Parse(Console.ReadLine());
-        int tirdNum = int.Parse(Console.ReadLine());
-        int fourthNum = int.Parse(Console.ReadLine());
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+        int thirdNumber = int.Parse(Console.ReadLine());
+        int fourthNumber = int.Parse(Console.ReadLine());
 
         //here print them on the console
         Console.WriteLine($"{firstNum:D4} {secondNum:D4} {tirdNum:D4} {fourthNum:D4}");
